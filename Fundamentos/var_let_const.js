7d58e254b47148b157e4428670d54ea5c03a5cab
Geovana Malta <geovanamalta2013@gmail.com>
2023-01-04 22:06:26 -0300

    pastas

---
+++ Fundamentos/var_let_const.js
@@ -0,0 +1,25 @@
+{ //var é variavel global, posso mudar a qualquer hora
+    var pessoa = "gabriela"
+    console.log(pessoa)
+
+    pessoa = "jessica"
+    console.log(pessoa)
+}
+
+{// let e a nova variavel padrao, tambem posso mudar a qualquer hora
+    let pessoa2 = "fernanda"
+    console.log(pessoa2)
+
+    pessoa2 = "katrina"
+    console.log(pessoa2)
+
+}
+
+{//const é constante, nao posso mudar o valor dela no codigo depois dela ja definida
+    const pessoa3 = "jamili"
+    console.log(pessoa3)
+
+    pessoa3 = "discordia"
+    console.log(pessoa3)
+}
+
