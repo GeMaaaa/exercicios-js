0597d9a5c9bbda8f337f7b4a49a525fd0f5a8f5f
Geovana Malta <geovanamalta2013@gmail.com>
2023-02-06 20:59:35 -0300

    questoes06_02

---
+++ exercicios/03.js
@@ -0,0 +1,8 @@
+//03) Crie uma função que recebe dois parâmetros, base e expoente, e retorne a base elevada ao expoente.
+
+let doisParametros = (base, expoente) => {
+    return base ** expoente
+}
+
+
+console.log(doisParametros(3, 2))
