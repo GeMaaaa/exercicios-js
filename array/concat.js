e64de8542666dfbb0ae97a701fb5fe96c31934f8
Geovana Malta <geovanamalta2013@gmail.com>
2023-03-14 00:05:15 -0300

    14_03

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['kai', 'yuri']
+const filhos = ['luana', 'fernando']
+const todos = filhas.concat(filhas)
+
+console.log(todos, filhas, filhos)
+
+console.log(['a',].concat([1, 2], [3, 4], 5, [[6, 7]]))
